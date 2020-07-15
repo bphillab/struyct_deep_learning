76ec3e6055ae00018e5ff1e3c2c84ccf9ba8e51a
Benjamin Phillabaum <bphillab@gmail.com>
2020-07-15 16:34:23 -0700

    trying this one

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
 def read_games_data(link= 'https://liveproject-resources.s3.amazonaws.com/other/deeplearningbasketballscores/Games.csv'):
-    return pd.read_csv(link,header=['game_date','team1','score1','team2','score2'])
+    return pd.read_csv(link,header=None, names=['game_date','team1','score1','team2','score2'])
 
 
 def read_teams_data(link='https://liveproject-resources.s3.amazonaws.com/other/deeplearningbasketballscores/Teams.csv'):
-    return pd.read_csv(link, header=['conf','team'])
+    return pd.read_csv(link,header=None, names=['conf','team'])
+
